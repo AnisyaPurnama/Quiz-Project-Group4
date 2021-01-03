37a352d28fc26e509153259b389a9664c51b5f00
Sharaf <airsol.enac@gmail.com>
2021-01-04 03:01:34 +0500

    added set-available listener

---
+++ src/listeners/set-available.js
@@ -0,0 +1,4 @@
+import { setAvailableQuestions } from "../handlers/set-available";
+import {setAvailableQuestions} from '../handlers/set-available.js;''
+
+document.getElementById('start-button').addEventListener('click', setAvailableQuestions);
