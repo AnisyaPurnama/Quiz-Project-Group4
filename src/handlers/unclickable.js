1a230b972449fd2d4fdfb082d92a4b845fb1ebff
Sharaf <airsol.enac@gmail.com>
2021-01-04 02:11:40 +0500

    added unclickable handler

---
+++ src/handlers/unclickable.js
@@ -0,0 +1,8 @@
+// Export unclickable function
+
+export function unclickableOptions(){
+    const optionLen = optionContainer.children.length;
+    for(let i=0; i<optionLen; i++){
+        optionContainer.children[i].classList.add("already-answered")
+    }
+}
