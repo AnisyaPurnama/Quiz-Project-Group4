d96cbb599694312496b3eab65e237dde2ba9836d
Sharaf <airsol.enac@gmail.com>
2021-01-04 02:16:43 +0500

    added handler for set available questions

---
+++ src/handlers/set-available.js
@@ -0,0 +1,9 @@
+// Export set available questions function
+
+export function setAvailableQuestions(){
+    const totalQuestion = quiz.length;
+    for(let i=0; i<totalQuestion; i++){
+        availableQuestions.push(quiz[i])
+    }
+}
+
