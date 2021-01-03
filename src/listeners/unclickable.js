2d895e7b70a7bf2be2ed87c63177676e756c8ff3
Sharaf <airsol.enac@gmail.com>
2021-01-04 03:40:06 +0500

    added listener for unclickable

---
+++ src/listeners/unclickable.js
@@ -0,0 +1,7 @@
+
+
+// Add listener of unclickable (Option Class)
+
+import { unclickableOptions } from "../handlers/unclickable";
+
+document.getElementsByClassName('option').addEventListener("click", unclickableOptions );
